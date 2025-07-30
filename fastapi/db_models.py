b18f12d6efc42339a5f87c0b2573f7487f446172
Narmin Huseynova <nerminrovshan@gmail.com>
2025-07-30 20:31:26 +0400

    db model

---
+++ fastapi/db_models.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, String, Integer, TIMESTAMP
+from database import base
+
+
+class books(base):
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    stars = Column(String)
+    stock = Column(String)
+    category = Column(String)
+    book_tax = Column(String)
+    number_reviews = Column(String)
+    page_url = Column(String)
+    loaded_at = Column(TIMESTAMP)
+
