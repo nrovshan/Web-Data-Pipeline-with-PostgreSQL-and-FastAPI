50e46820b623cb5c6bf0bd9ddec5e9e1ee6a4647
Narmin Huseynova <nerminrovshan@gmail.com>
2025-08-05 19:30:25 +0400

    fastapi

--- fastapi/routers/books.py
+++ fastapi/routers/books.py
@@ -1,7 +1,12 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy.orm import session
+from sqlalchemy.orm import Session
 from ..database import get_db  
-from .. import db_models
+from ..db_models import cleanbooks
 
 router = APIRouter()
+
+@router.get("/books")
+def get_books(db: Session = Depends(get_db)):
+    books = db.query(cleanbooks).all()
+    return books
 
