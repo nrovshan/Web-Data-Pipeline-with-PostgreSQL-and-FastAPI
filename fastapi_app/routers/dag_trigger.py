47ebe9c73b81abca39b73f882b34fed8133c2be1
Narmin Huseynova <nerminrovshan@gmail.com>
2025-08-06 01:13:10 +0400

    dag trigger1

--- fastapi_app/routers/dag_trigger.py
+++ fastapi_app/routers/dag_trigger.py
@@ -1,7 +1,17 @@
 import subprocess
+from fastapi import APIRouter
+
+router = APIRouter()
 
 def dag_trigger(dag_id: str):
     result = subprocess.run(["docker", "exec", "-i", "auto_pipe_project-airflow-scheduler-1", "airflow", "dags", "trigger", dag_id],
                             capture_output=True, text=True)
     return result.stdout or result.stderr
+
+
+@router.post("/run_dag/{dag_id}")
+def run_dag(dag_id: str):
+    output = dag_trigger(dag_id)
+
+    return {"message": f"DAG {dag_id} triggered", "output": output}
     
